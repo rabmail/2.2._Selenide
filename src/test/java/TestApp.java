d2f016ce88580dc5c2e45cddfdbc955a1e9e71c7
Dmitriy Dolmatov <Rabmail@mail.ru>
2021-03-27 10:25:41 +0300

    Edit Test

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -9,12 +9,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class testApp {
+public class TestApp {
 
-      public String SetDate (int date){
-      return LocalDate.now().plusDays(date).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public String setDate (int date){
+        return LocalDate.now().plusDays(date).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @BeforeEach
@@ -25,20 +26,19 @@ public class testApp {
     @Test
     void shouldTestValidation() {
         $("[data-test-id=city] input").setValue("Белгород");
-        $("[data-test-id=date] input").setValue(SetDate(3));
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL,Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(setDate(4));
         $("[data-test-id=name] input").setValue("Пупкин Василий");
         $("[data-test-id=phone] input").setValue("+79102280000");
         $("[data-test-id=agreement]").click();
         $$("button").get(1).click();
-        //$("[data-test-id='notification']").waitUntil(visible, 15000).shouldHave(text("Успешно!"));
-        //$("[data-test-id=notification]").shouldHave((visible),Duration.ofSeconds(15));
-        //$(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+         //$(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id=notification]").shouldBe((visible), Duration.ofSeconds(15));
     }
     @Test
     void shouldTestWorkingDropDownMenuCity() {
         $("[data-test-id=city] input").sendKeys("Бе", Keys.ARROW_DOWN, Keys.ENTER);
-        $("[data-test-id=date] input").setValue(SetDate(3));
+        $("[data-test-id=date] input").setValue(setDate(3));
         $("[data-test-id=name] input").setValue("Пупкин Василий");
         $("[data-test-id=phone] input").setValue("+79102280000");
         $("[data-test-id=agreement]").click();
@@ -49,10 +49,23 @@ public class testApp {
     void shouldTestWorkingDropDownMenuDate() {
         $("[data-test-id=city] input").sendKeys("Бе", Keys.ARROW_DOWN, Keys.ENTER);
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL,Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(SetDate(7));
+        $("[data-test-id=date] input").setValue(setDate(7));
         $("[data-test-id=name] input").setValue("Пупкин Василий");
         $("[data-test-id=phone] input").setValue("+79102280000");
         $("[data-test-id=agreement]").click();
         $$("button").get(1).click();
     }
+    @Test
+    void shouldTestValidationData() {
+        String  dateTest = setDate(4);
+        $("[data-test-id=city] input").setValue("Белгород");
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL,Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(setDate(4));
+        $("[data-test-id=name] input").setValue("Пупкин Василий");
+        $("[data-test-id=phone] input").setValue("+79102280000");
+        $("[data-test-id=agreement]").click();
+        $$("button").get(1).click();
+        $(withText(dateTest)).shouldBe(visible, Duration.ofSeconds(15));
+    }
 }
+
